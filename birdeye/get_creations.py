ff833c657687b887ef67c16d772d4ea55021ced7
Brett Falk <fbrett@cis.upenn.edu>
2025-02-03 18:05:24 -0500

    got get_creation working

--- birdeye/get_creations.py
+++ birdeye/get_creations.py
@@ -3,6 +3,8 @@ import os
 from pathlib import Path
 import csv
 import pandas as pd
+from tqdm import tqdm
+import time
 
 data_path = Path("../bitquery/data")
 
@@ -29,18 +31,71 @@ except Exception as e:
 
 address = "D7rcV8SPxbv94s3kJETkrfMrWqHFs6qrmtbiu6saaany"
 base_url = f"https://public-api.birdeye.so/defi/token_creation_info"
-url = f"{base_url}?address={address}"
+outfile = "data/token_creation_info.csv"
+errfile = "birdeye_errors.csv"
+max_retries = 3
 
-headers = {
-    "accept": "application/json",
-    "x-chain": "solana",
-    "X-API-KEY": api_key
-}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    print(response.text)
+if os.path.exists( outfile ):
+    df = pd.read_csv( outfile )
+    completed_addresses = set( df.tokenAddress )
 else:
-    print(f"Error: {response.status_code}")
-    print(response.text)
+    completed_addresses = set()
+
+def get_token_creation_info( address, retry=0, wait=5):
+    url = f"{base_url}?address={address}"
+
+    headers = {
+        "accept": "application/json",
+        "x-chain": "solana",
+        "X-API-KEY": api_key
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+    except Exception as e:
+        with open( errfile, "a" ) as f:
+            f.write(f"{address},{e}\n")
+        if retry < max_retries:
+            time.sleep( wait )
+            return get_token_creation_info( address, retry+1, wait*2 )
+        else:
+            return None
+
+    if response.status_code == 200:
+        try:
+            json_response = response.json()
+            return json_response['data']
+        except Exception as e:
+            with open( errfile, "a" ) as f:
+                f.write(f"{address},{e}\n")
+            if retry < max_retries:
+                time.sleep( wait )
+                return get_token_creation_info( address, retry+1, wait*2 )
+            else:
+                return None
+        #{"data":{"txHash":"3cW2HpkUs5Hg2FBMa52iJoSMUf8MNkkzkRcGuBs1JEesQ1pnsvNwCbTmZfeJf8hTi9NSHh1Tqx6Rz5Wrr7ePDEps","slot":223012712,"tokenAddress":"D7rcV8SPxbv94s3kJETkrfMrWqHFs6qrmtbiu6saaany","decimals":5,"owner":"JEFL3KwPQeughdrQAjLo9o75qh15nYbFJ2ZDrb695qsZ","blockUnixTime":1697044029,"blockHumanTime":"2023-10-11T17:07:09.000Z"},"success":true}
+    else:
+        with open( errfile, "a" ) as f:
+            f.write(f"{address},{response.status_code}\n")
+        if retry < max_retries:
+            time.sleep( wait )
+            return get_token_creation_info( address, retry+1, wait*2 )
+        else:
+            return None
+
+for address in tqdm(set(token_addresses).difference(completed_addresses)):
+    info = get_token_creation_info( address )
+
+    if Path( outfile ).is_file():
+        with open( outfile, "a" ) as f:
+            dw = csv.DictWriter( f, info.keys() )
+            dw.writerow( info )
+    else:
+        with open( outfile, "w" ) as f:
+            dw = csv.DictWriter( f, info.keys() )
+            dw.writeheader()
+            dw.writerow( info )
+    
+    time.sleep( 1 )
+
+
